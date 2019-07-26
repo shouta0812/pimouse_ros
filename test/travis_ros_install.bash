63f5d1d4640b8f87f07fe552670aa2525251d47d
shouta0812 <shouta3012@ezweb.ne.jp>
2019-07-26 09:06:02 +0000

    cleanup

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,24 @@
+#!/bin/bash -xve
+
+#required packages
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospy
+sudo bundle install
+sudo gem install
+
+#ros install
+cd ..
+git clone git@github.com:shouta0812/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
+bash ./step0.bash
+bash ./step1.bash
+
+#catkin setup
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+source /opt/ros/indigo/setup.bash
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
