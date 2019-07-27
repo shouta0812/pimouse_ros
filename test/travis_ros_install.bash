f0632bc70359dcc308f13a28b60c2802ad5feec1
shouta0812 <shouta3012@ezweb.ne.jp>
2019-07-27 09:20:03 +0000

    cealnup

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -12,7 +12,7 @@ bash ./step1.bash
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 source /opt/ros/indigo/setup.bash
+PYTHONPATH="/home/ubuntu/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages"$PYTHONPATH
 catkin_init_workspace
 cd ~/catkin_ws
-env | grep python
 catkin_make
