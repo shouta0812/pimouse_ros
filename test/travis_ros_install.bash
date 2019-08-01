6e8256ca03a39ffabb050abd6307397d71717e7d
shouta0812 <shouta3012@ezweb.ne.jp>
2019-08-01 04:54:27 +0000

    cleanup

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -11,8 +11,9 @@ sudo pip install rospkg
 sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $ROS_CI_DESKTOP main\" > /etc/apt/sources.list.d/ros-latest.list"
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update -qq
-sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool ros-kinetic-desktop-full
-source /opt/ros/kinetic/setup.bash
+ros base
+sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-desktop-full
+source /opt/ros/$ROS_DISTRO/setup.bash
 sudo rosdep init
 rosdep update
 
