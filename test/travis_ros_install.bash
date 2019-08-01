45eb536f3b5d134a1c0acdf9c46be726f07e1dfe
shouta0812 <shouta3012@ezweb.ne.jp>
2019-08-01 05:01:09 +0000

    cleanup

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -8,20 +8,20 @@ sudo pip install pyyaml
 sudo pip install rospkg
 
 #ros install
-sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $ROS_CI_DESKTOP main\" > /etc/apt/sources.list.d/ros-latest.list"
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt-get update -qq
-ros base
-sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-desktop-full
-source /opt/ros/$ROS_DISTRO/setup.bash
-sudo rosdep init
-rosdep update
+-sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu $ROS_CI_DESKTOP main\" > /etc/apt/sources.list.d/ros-latest.list"
+-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+-sudo apt-get update -qq
+-ros base
+-sudo apt-get install -y python-catkin-pkg python-rosdep python-wstool ros-$ROS_DISTRO-desktop-full
+-source /opt/ros/$ROS_DISTRO/setup.bash
+-sudo rosdep init
+-rosdep update
 
 #catkin setup
-mkdir -p ~/catkin_ws/src
-cd ~/catkin_ws/src
-source /opt/ros/kinetic/setup.bash
-catkin_init_workspace
-cd ~/catkin_ws
-catkin_make
+-mkdir -p ~/catkin_ws/src
+-cd ~/catkin_ws/src
+-source /opt/ros/kinetic/setup.bash
+-catkin_init_workspace
+-cd ~/catkin_ws
+-catkin_make
 
